f655b7d6f050fc0fd29a55fce8d2a1b57ae652f2
Jamkhan11 <jamshid.rahwardak@gmail.com>
2023-05-11 09:14:02 +0200

    use strict -

--- script_01.js
+++ script_01.js
@@ -3,3 +3,16 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+test();
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
+function test()
+{
+    console.log("Hi schöne!");
+}
+
+
+test();
+test();
